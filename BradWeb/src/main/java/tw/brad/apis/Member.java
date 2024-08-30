b26487af5fa6b9ee987b755776e33f5b2eb22e7f
Jack8888 <dx25201469@yahoo.com.tw>
2024-08-30 11:57:51 +0800

    08301200上課內容

---
+++ BradWeb/src/main/java/tw/brad/apis/Member.java
@@ -0,0 +1,45 @@
+
+package tw.brad.apis;
+
+public class Member {
+	private int id;
+	private String account, passwd, name;
+	private String icon;	// base64-format
+
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%d : %s", id, name);
+	}
+	
+}
